2bf5227a93ac4f61b54d7cb57d3a62dfac8096a5
Yoennis Rodriguez Gonzalez <78815388+yoennisrg@users.noreply.github.com>
2022-04-29 13:26:03 -0400

    define element numbers

--- src/seeding/user.seeder.ts
+++ src/seeding/user.seeder.ts
@@ -3,11 +3,6 @@ import { Factory, Seeder } from 'typeorm-seeding';
 
 export default class CreateUser implements Seeder {
   public async run(factory: Factory): Promise<any> {
-    await factory(User)().create({
-      firstName: 'John',
-      lastName: 'Smith',
-      age: 23,
-      isActive: false,
-    });
+    await factory(User)().createMany(10);
   }
 }
